650ebb2fb148f579464a2048086ab6a2c34a6600
Walmir <owalmirneto@gmail.com>
2025-03-07 09:57:46 -0300

    Add url validation (#3)
    * feat: create url validation
    
    * feat: add validation by domain
    
    * docs: url validation

---
+++ lib/validators/url_validator.rb
@@ -0,0 +1,28 @@
+require "uri"
+
+class UrlValidator < Validaty::AllowBlankBase
+  def validate_each(resource, attribute, value)
+    if options[:domain].present? && URI.parse(value).host != options[:domain]
+      resource.errors.add(attribute, :domain, domain: options[:domain])
+    end
+
+    if options[:starts_with].present? && !value.starts_with?(options[:starts_with])
+      resource.errors.add(attribute, :starts_with, start: options[:starts_with])
+    end
+
+    super
+  end
+
+  private
+
+  def valid_value?(url)
+    uri = URI.parse(url)
+    uri.host.present?
+  rescue URI::InvalidURIError
+    false
+  end
+
+  def default_message_error
+    :invalid
+  end
+end
